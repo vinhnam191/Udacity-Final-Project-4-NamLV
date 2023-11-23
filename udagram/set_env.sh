8c5b3269272b2933be4afe2e6c9a68ab08297135
vinhnam191 <vinhnam191@gmail.com>
2023-11-24 01:17:48 +0700

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=namlv18
+export POSTGRES_PASSWORD=namle23032307
+export POSTGRES_HOST=postgres.cj3ww3skvksp.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::namlv18finalprojectudacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
