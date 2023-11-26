36313f3d0bcd9a9e4ba1881b99c85e5e57d13e58
vinhnam191 <vinhnam191@gmail.com>
2023-11-26 21:29:32 +0700

    formated

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,5 +1,5 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
 
 export const sequelize = new Sequelize({
   username: config.username,
@@ -7,6 +7,6 @@ export const sequelize = new Sequelize({
   database: config.database,
   host: config.host,
 
-  dialect: "postgres",
-  storage: ":memory:",
+  dialect: 'postgres',
+  storage: ':memory:',
 });
